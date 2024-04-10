67fc66ceedac94a9062e30be991801884addb0f4
ninaadkiran <ninaadkiran@gmail.com>
2024-04-10 20:10:23 +0000

    port change

--- main.py
+++ main.py
@@ -75,4 +75,4 @@ app.cli.add_command(custom_cli)
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
-    app.run(debug=True, host="0.0.0.0", port="8058")
+    app.run(debug=True, host="0.0.0.0", port="8059")
